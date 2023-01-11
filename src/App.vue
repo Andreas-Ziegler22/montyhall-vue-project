51a641698aff6cd57ee166e923a961a498e0f19e
MrBurp <andreass.ziegler@gmail.com>
2023-01-11 10:53:56 +0100

    #app styled

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,25 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+* {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
+body {
+  color: #fff;
+  background: linear-gradient(to right, #6a9113, #141517);
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h1 {
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
+}
+</style>
