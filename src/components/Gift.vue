cca0ee3e3998dc0b20b7a3a2bf7b8a1417d61f75
MrBurp <andreass.ziegler@gmail.com>
2023-01-11 11:36:26 +0100

    gift style

--- src/components/Gift.vue
+++ src/components/Gift.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="gift">
     <div class="gift-tie-1"></div>
     <div class="gift-tie-2"></div>
     <div class="gift-top"></div>
@@ -13,4 +13,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.gift {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
