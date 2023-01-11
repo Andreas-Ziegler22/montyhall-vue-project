77cee8e07d6a36a70646b7a48fbd24447fdd891f
MrBurp <andreass.ziegler@gmail.com>
2023-01-11 14:58:32 +0100

    component door with number and may have gift

---
+++ src/components/Door.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+import Gift from "./Gift.vue";
+
+export default {
+  name: "Door-componet",
+  comments: { Gift },
+  props: {
+    number: {},
+    hasGift: { type: Boolean },
+  },
+};
+</script>
+
+<style></style>
