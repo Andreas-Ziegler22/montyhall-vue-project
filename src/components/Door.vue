55fbea5948880abb80cd7434e746668b5476ef86
MrBurp <andreass.ziegler@gmail.com>
2023-01-12 19:13:46 +0100

    door when selected

--- src/components/Door.vue
+++ src/components/Door.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="door-area">
-    <div class="door-frame">
+    <div class="door-frame" :class="{ selected }">
       <Gift v-if="open && hasGift" />
     </div>
-    <div class="door">
-      <div class="number">{{ number }}</div>
-      <div class="knobDoor"></div>
+    <div class="door" @click="selected = !selected">
+      <div class="number" :class="{ selected }">{{ number }}</div>
+      <div class="knobDoor" :class="{ selected }"></div>
     </div>
   </div>
 </template>
@@ -32,6 +32,7 @@ export default {
 <style>
 :root {
   --door-border: 5px solid brown;
+  --selected-border: 5px solid yellow;
 }
 
 .door-area {
@@ -79,5 +80,19 @@ export default {
   background-color: brown;
   align-self: flex-start;
   margin-top: 60px;
+}
+
+.door-frame.selected {
+  border-left: var(--selected-border);
+  border-top: var(--selected-border);
+  border-right: var(--selected-border);
+}
+
+.door > .number.selected {
+  color: yellow;
+}
+
+.door > .knobDoor.selected {
+  background-color: yellow;
 }
 </style>
